84004f6e951a9ef7dcf9c8b7c4b48d360ff6e6bf
dcancino <d1990c@gmail.com>
2017-02-22 20:42:34 -0600

    Added files after testing

---
+++ impl/Tank.java
@@ -0,0 +1,30 @@
+package zombiewar.impl;
+import zombiewar.intf.ISurvivor;
+import zombiewar.intf.IZombie;
+
+public class Tank extends Character implements IZombie {
+	private static int objCount = 0;
+	private int id=0;
+	
+	/**
+	 * A tank starts with 150 points of health
+	 */
+	public Tank() {
+		super(150);
+		id = objCount++;
+	}
+
+	/**
+	 * An attack from the tank decrease a survivor's health by 20 points.
+	 * 
+	 */
+	@Override
+	public void attack(ISurvivor survivor) {
+		survivor.decreaseHealth(20);
+	}
+	
+	public String toString() {
+		return "Tank " + id;
+	}
+
+}
