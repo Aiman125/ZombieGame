68282d7d39632d8680e791519bdaa5925035132c
Ramnjit S. Boparai <ramnjit@hotmail.com>
2017-02-22 19:56:43 -0600

    uploaded Tank

---
+++ impl/Tank.java
@@ -0,0 +1,30 @@
+package Lab5.impl;
+import Lab5.intf.ISurvivor;
+import Lab5.intf.IZombie;
+
+public class Tank extends Character implements IZombie {
+	private static int objCount = 0;
+	private int id=0;
+	
+	/**
+	 * A tank starts with 150 points of health
+	 */
+	public Tank() {
+		super(150);
+		id = objCount++;
+	}
+
+	/**
+	 * An attack from the tank decrease a survivor's health by 20 points.
+	 * @param survivor 
+	 */
+	@Override
+	public void attack(ISurvivor survivor) {
+		survivor.decreaseHealth(20);
+	}
+	
+	public String toString() {
+		return "Tank " + id;
+	}
+
+}
