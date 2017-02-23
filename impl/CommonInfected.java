f3e40c4266fb01a75c03f8d4150e9dc994e457f1
seanrohde <rohdesm03@uww.edu>
2017-02-22 18:24:49 -0600

    common infected class

---
+++ impl/CommonInfected.java
@@ -0,0 +1,22 @@
+package zombiegame.impl;
+import zombiegame.intf.ISurvivor;
+import zombiegame.intf.IZombie;
+
+public class CommonInfected extends Character implements IZombie {
+	private static int objCount = 0;
+	private int id=0;
+	
+	public CommonInfected() {
+		super(30);
+		id = objCount++;
+	}
+
+	@Override
+	public void attack(ISurvivor survivor) {
+		survivor.decreaseHealth(5);
+	}
+	
+	public String toString(){
+		return "CommonInfected " + id;
+	}
+}
